b94270cc7467ae10a28343d13a20b019ceb55f95
Sami Onur Zaim <samionurzaim@gmail.com>
2021-05-20 17:44:58 +0300

    Create deplyZabbixAgent.sh

---
+++ deplyZabbixAgent.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+mkdir -p /etc/zabbix/scripts
+sed -i s/"Server=127.0.0.1"/"Server=zabbix.sld.gr"/g /etc/zabbix/zabbix_agent2.conf
+sed -i s/"ServerActive=127.0.0.1"/"ServerActive=zabbix.sld.gr"/g /etc/zabbix/zabbix_agent2.conf
+sed -i s/"Hostname=Zabbix server"/"ServerActive=kakofonix.sld.gr"/g /etc/zabbix/zabbix_agent2.conf
+
+cp smartctl-disks-discovery.pl /etc/zabbix/scripts
+cp apacheConnection.conf  /etc/zabbix/zabbix_agent2.d/
+cp mdadm.conf /etc/zabbix/zabbix_agent2.d/
+cp smartctl.conf /etc/zabbix/zabbix_agent2.d/
+cp userparameter_mysql.conf /etc/zabbix/zabbix_agent2.d/
+cp zabbix_smartctl.conf /etc/zabbix/zabbix_agent2.d/
+
+chmod +x /etc/zabbix/scripts -R
+chown zabbix:zabbix /etc/zabbix/scripts -R
+chmod u+s /usr/sbin/smartctl
+
+netstatCmd=$(which netstat)
+systemctlCmd=$(which systemctl)
+smartctlCmd=$(which smartctl)
+
+tee -a /etc/sudoers >/dev/null << EOF
+zabbix  ALL=(ALL) NOPASSWD: $netstatCmd
+zabbix  ALL=(ALL) NOPASSWD: $systemctlCmd
+zabbix  ALL=(ALL) NOPASSWD: $smartctlCmd
+zabbix  ALL=(ALL) NOPASSWD: /etc/zabbix/scripts/smartctl-disks-discovery.pl
+EOF
+
+systemctl enable --now zabbix-agent2
+systemctl restart zabbix-agent2
