e984019b3e33a839c62d76964f8262f2cfeecde4
Sami Onur Zaim <samionurzaim@gmail.com>
2022-05-31 20:28:00 +0300

    Create deploySmartctl.sh

---
+++ deploySmartctl.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+zabbixConfDir="/etc/zabbix"
+zabbixUserParamDir="zabbix_agentd.d"
+zabbixScriptDir="scripts"
+agentName="zabbix-agent"
+
+$zabbixConfDir/$zabbixUserParamDir
+
+mkdir -p $zabbixConfDir/$zabbixScriptDir
+
+cp smartctl-disks-discovery.pl $zabbixConfDir/$zabbixScriptDir
+cp mdadm.conf $zabbixConfDir/$zabbixUserParamDir
+cp smartctl.conf $zabbixConfDir/$zabbixUserParamDir
+cp zabbix_smartctl.conf $zabbixConfDir/$zabbixUserParamDir
+
+smartctlCmd=$(which smartctl)
+
+chmod +x $zabbixConfDir/$zabbixScriptDir -R
+chown zabbix:zabbix $zabbixConfDir/$zabbixScriptDir -R
+chmod u+s $smartctlCmd
+
+smartctlCmd=$(which smartctl)
+
+tee -a /etc/sudoers.d/zabbix >/dev/null << EOF
+zabbix  ALL=(ALL) NOPASSWD: $netstatCmd
+zabbix  ALL=(ALL) NOPASSWD: $systemctlCmd
+zabbix  ALL=(ALL) NOPASSWD: $smartctlCmd
+zabbix  ALL=(ALL) NOPASSWD: $zabbixConfDir/$zabbixScriptDir/smartctl-disks-discovery.pl
+EOF
+
+systemctl enable --now $agentName
+systemctl restart $agentName
