a4884095f01468df4c71e0a0b4cf8746072a5eb8
Sami Onur Zaim <samionurzaim@gmail.com>
2022-06-01 17:24:19 +0300

    Create temp.sh

---
+++ temp.sh
@@ -0,0 +1,25 @@
+apt install lm-sensors
+
+zabbixConfDir="/etc/zabbix"
+zabbixUserParamDir="zabbix_agentd.d"
+zabbixScriptDir="scripts"
+agentName="zabbix-agent"
+
+mkdir -p $zabbixConfDir/$zabbixScriptDir
+
+cp cpuTemp.conf $zabbixConfDir/$zabbixScriptDir
+
+sensorCmd=$(which sensors)
+
+chmod +x $zabbixConfDir/$zabbixScriptDir -R
+chown zabbix:zabbix $zabbixConfDir/$zabbixScriptDir -R
+chmod u+s $sensorCmd
+
+smartctlCmd=$(which sensorCmd)
+
+tee -a /etc/sudoers.d/zabbix >/dev/null << EOF
+zabbix  ALL=(ALL) NOPASSWD: $sensorCmd
+EOF
+
+systemctl enable --now $agentName
+systemctl restart $agentName
